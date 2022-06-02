2bb48b50c5289c1fbe0b768cc0b52ef0773b4912
proserven <proserven@users.noreply.github.com>
2022-06-02 00:45:50 -0400

    update

--- src/main/java/com/revature/controllers/OrderController.java
+++ src/main/java/com/revature/controllers/OrderController.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 public class OrderController {
 
     @Autowired
-    OrderRepository orderRepository;
+    private OrderRepository orderRepository;
 
     @Autowired
     ProductRepository productRepository;
